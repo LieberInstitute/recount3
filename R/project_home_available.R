2aa9e6d5fc375d48b2f63d0289e7e4e0e83e9ef4
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-10-14 12:00:22 -0400

    Remove file_list() since it's not really of use given that we can't list files at IDIES unlike the test URL.

--- R/project_home_available.R
+++ R/project_home_available.R
@@ -24,23 +24,44 @@
 #' )
 project_home_available <-
     function(organism = c("human", "mouse"),
-    recount3_url = "http://idies.jhu.edu/recount3/data",
-    bfc = BiocFileCache::BiocFileCache()) {
+        recount3_url = "http://idies.jhu.edu/recount3/data",
+        bfc = BiocFileCache::BiocFileCache()) {
         organism <- match.arg(organism)
 
         if (recount3_url == "http://idies.jhu.edu/recount3/data") {
             if (organism == "mouse") {
                 return("data_sources/sra")
             } else if (organism == "human") {
-                return(c("data_sources/sra", "data_sources/gtex", "data_sources/tcga"))
+                return(c(
+                    "data_sources/sra",
+                    "data_sources/gtex",
+                    "data_sources/tcga"
+                ))
             }
+        } else if (recount3_url == "http://snaptron.cs.jhu.edu/data/temp/recount3") {
+            if (organism == "mouse") {
+                return("data_sources/sra")
+            } else if (organism == "human") {
+                return(
+                    c(
+                        "data_sources/sra",
+                        "data_sources/gtex",
+                        "collections/ccle",
+                        "collections/geuvadis_smartseq",
+                        "collections/gtex_geuvadis"
+                    )
+                )
+            }
+        } else if (!file.exists(recount3_url)) {
+            stop(
+                "'recount3_url' is not a valid URL or is it not an existing directory in your file system.",
+                call. = FALSE
+            )
         }
 
         ## Define the base directories
-        base_dirs <- c(
-            "data_sources",
-            "collections"
-        )
+        base_dirs <- c("data_sources",
+            "collections")
         if (organism == "mouse") {
             ## Currently there are no mouse collections
             base_dirs <- "data_sources"
@@ -48,11 +69,10 @@ project_home_available <-
 
         homes <- lapply(base_dirs, function(base_dir) {
             ## Build the query URL
-            url <- file.path(recount3_url, organism, base_dir)
-            names(url) <- url
+            path <- file.path(recount3_url, organism, base_dir)
 
             ## Find the available options
-            available <- file_list(url, bfc = bfc)
+            available <- dir(path)
 
             ## Add the paths
             file.path(base_dir, available)
