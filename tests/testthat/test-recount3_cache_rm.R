374dac22a478a19d6851f5f5bbda11110ef2b3f5
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-10-16 18:13:39 -0400

    v0.99.8 -- try again

--- tests/testthat/test-recount3_cache_rm.R
+++ tests/testthat/test-recount3_cache_rm.R
@@ -3,11 +3,11 @@ test_url <- "http://idies.jhu.edu/recount3/data/human/data_sources/sra/metadata/
 
 BiocFileCache::bfcrpath(
     temp_bfc_rm,
-
     test_url
 )
 
 test_that("Wiping out the cache works", {
+    recount3_cache_rm(temp_bfc_rm)
     expect_equal(
         recount3_cache_files(temp_bfc_rm),
         test_url
