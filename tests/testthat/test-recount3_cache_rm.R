c33829c00ada7a345b1fb0cd46271a3f77c21245
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-10-16 16:21:30 -0400

    v0.99.6 -- try fixing the rm test

--- tests/testthat/test-recount3_cache_rm.R
+++ tests/testthat/test-recount3_cache_rm.R
@@ -1,16 +1,18 @@
-temp_bfc <- recount3_cache(file.path(tempdir(), "recount3_cache_rm"))
+temp_bfc_rm <- recount3_cache(file.path(tempdir(), "recount3_cache_rm"))
 test_url <- "http://idies.jhu.edu/recount3/data/human/data_sources/sra/metadata/35/DRP002835/sra.recount_project.DRP002835.MD.gz"
 
 BiocFileCache::bfcrpath(
-    temp_bfc,
+    temp_bfc_rm,
+    
     test_url
 )
 
 test_that("Wiping out the cache works", {
+    recount3_cache_rm(temp_bfc_rm)
     expect_equal(
-        recount3_cache_files(temp_bfc),
+        recount3_cache_files(temp_bfc_rm),
         test_url
     )
-    recount3_cache_rm(temp_bfc)
-    expect_equal(recount3_cache_files(temp_bfc), character())
+    recount3_cache_rm(temp_bfc_rm)
+    expect_equal(recount3_cache_files(temp_bfc_rm), character())
 })
